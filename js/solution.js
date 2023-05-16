becb5f5f70fffb7c25ac45a26be8e7f215e343af
Olga <olga1998senko@gmail.com>
2023-05-16 09:39:17 +0300

    dom_project43

--- js/solution.js
+++ js/solution.js
@@ -1,22 +1,20 @@
 "use strict";
-const ul = document.querySelector('ul');
-const arrAttribute = [];
-const arrValue = [];
-for(const value of ul.attributes) {
-    arrValue.push(value.value);
+const table = document.createElement('table');
+table.setAttribute('main-table', '')
+const rows = 10;
+const cols = 10;
+for(let i = 1; i <= rows; i++) {
+    const row = document.createElement('tr');
+    for(let j = 1; j <= cols; j++) {
+        const col = document.createElement('td');
+        let mult = i * j;
+        let result = document.createTextNode(mult);
+        col.appendChild(result);
+        row.appendChild(col);
+    }
+    table.appendChild(row);
 }
-console.log(arrValue);
-for(const list of ul.attributes) {
-    arrAttribute.push(list);
-}
-console.log(arrAttribute);
-const liLast = document.querySelectorAll('li');
-const lastLength = liLast[liLast.length - 1];
-lastLength.textContent = 'My name is Olga';
-console.log(lastLength);
-const firstElement = document.querySelector('li');
-firstElement.setAttribute('data-my-name', 'Olga');
-console.log(firstElement);
-ul.removeAttribute('data-dog-tail');
-console.log(ul);
+document.body.appendChild(table);
+
+
 
